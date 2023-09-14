0f07cd1e90a1e9034d7b03aa734af192490f2baf
Victor Martinez <vicmtzdev@gmail.com>
2023-09-14 02:24:20 -0600

    first-commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+require('dotenv').config();
+
+
+// Crear el servidor de express
+const app = express();
+
+
+//Directorio público
+app.use( express.static('public') );
+
+
+// Lectura y parseo del body
+app.use( express.json() );
+
+
+// Rutas
+app.use('/api/auth', require('./routes/auth'));
+// TODO: CRUD
+
+
+// Escuchar peticiones
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
+});
---
+++ routes/auth.js
@@ -0,0 +1,38 @@
+
+// Rutas Auth: host + /api/auth/
+
+const { Router } = require('express');
+const { check } = require('express-validator');
+
+const { validarCampos } = require('../middlewares/validar-campos');
+const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
+
+const router = Router();
+
+
+router.post(
+    '/new', 
+    [
+        check('name', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'El password debe contener el menos 6 caracteres').isLength({ min: 6 }),
+        validarCampos
+    ],
+    crearUsuario 
+);
+
+
+router.post(
+    '/', 
+    [
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'El password debe contener el menos 6 caracteres').isLength({ min: 6 }),
+        validarCampos
+    ],
+    loginUsuario 
+);
+
+
+router.get('/renew', revalidarToken );
+
+module.exports = router;
